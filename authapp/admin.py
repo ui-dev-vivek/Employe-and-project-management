bbadbfcf482437bd4f3505113d9fdbc62535f419
Vivek Yadav <vivek.k@prarang.in>
2024-10-03 17:55:33 +0530

    new

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from authapp.models import User
+from django import forms
 
+class MyUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'is_employee', 'is_client')
+        
+    def __init__(self, *args, **kwargs):
+        super(MyUserForm, self).__init__(*args, **kwargs)      
+        self.fields['username'].required = True   
+        self.fields['email'].required = True      
+        self.fields['first_name'].required = True 
+        self.fields['last_name'].required = True  
+        self.fields['is_employee'].required = False 
+        self.fields['is_client'].required = False
+        self.fields['date_joined'].required=False   
 
+    
 
 class MyUser(admin.ModelAdmin):
+    form = MyUserForm
     list_display=('username','email','is_superuser','is_employee','is_client')
     fieldsets=(
         ('Basic',{'fields':('username','password')}),
@@ -12,4 +29,7 @@ class MyUser(admin.ModelAdmin):
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
         ('Custom Fields', {'fields': ('is_employee', 'is_client')}),
     )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+    
 admin.site.register(User,MyUser)
--- authapp/models.py
+++ authapp/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class User(AbstractUser):
     is_employee=models.BooleanField(default=False)
     is_client=models.BooleanField(default=False)
+    date_joined = models.DateTimeField(null=True, blank=True)
     # pass
     def __str__(self):
         return self.username
