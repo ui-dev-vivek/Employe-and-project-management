fb3685888b497f6c15f7e1064fad3c63f47c7473
ASG Kali <dev.vivek16@gmail.com>
2024-10-03 07:34:50 +0530

    Authapp

--- config/models.py
+++ config/models.py
@@ -5,6 +5,7 @@ class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
